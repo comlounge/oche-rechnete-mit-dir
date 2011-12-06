da35ea63c9733562ef7d5ff4c0c7153df9305cbb
Christian Scholz <cs@comlounge.net>
2011-12-06 21:55:09 +0100

    added setup and ignore

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+import sys, os
+
+version = '0.9'
+
+setup(name='aachen-haushalt',
+      version=version,
+      description="Aufbereitung Aachener Haushaltsbefragung",
+      long_description="""\
+""",
+      classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      keywords='',
+      author='COM.lounge',
+      author_email='info@comlounge.net',
+      url='',
+      license='MIT',
+      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+        "starflyer",
+        "mechanize",
+        "pymongo",
+        "BeautifulSoup"
+      ],
+      entry_points="""
+      """,
+      )
